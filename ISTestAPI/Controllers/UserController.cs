9edabb173caad4d193d99efe27abae49aa5162a0
Ashan Maduwantha <ashanmaduwantha99@gmail.com>
2023-01-31 07:26:10 +0530

    initial setup

---
+++ ISTestAPI/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using ISTestAPI.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISTestAPI.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        
+        [HttpGet]
+        [Route("user/list")]
+        public async Task<List<User>> GetAllUser()
+        {
+            var users = new List<User>();
+
+            var user1 = new User
+            {
+                Id = 1,
+                Email = "user1@email.com",
+                Name = "user 1"
+            };
+
+            var user2 = new User
+            {
+                Id = 2,
+                Email = "user2@email.com",
+                Name = "user 2"
+            };
+
+            users.Add(user1);
+            users.Add(user2);
+
+
+            return users;
+        }
+    }
+}
---
+++ ISTestAPI/DTO/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ISTestAPI.DTO
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
